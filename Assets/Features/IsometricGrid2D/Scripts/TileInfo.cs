60c6e921b1b3869a16aeb3cd694542df4828a1ca
Saad Asif <saad20asif@gmail.com>
2024-04-21 16:35:04 +0500

    Getters Setter For Tile Info Properties!

--- Assets/Features/IsometricGrid2D/Scripts/TileInfo.cs
+++ Assets/Features/IsometricGrid2D/Scripts/TileInfo.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TileInfo : MonoBehaviour
 {
-    public int TileType = 0;
-    public Vector2Int MyIndex;
-    public bool Filled = false;
+    public int TileType { get; set; } = 0;
+    public Vector2Int MyIndex { get; set; }
+    public bool Filled { get; set; } = false;
 }
