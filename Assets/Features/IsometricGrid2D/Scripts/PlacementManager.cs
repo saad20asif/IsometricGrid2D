fc233f8789c1f9562c235110fd0517d8a336e63b
Saad Asif <saad20asif@gmail.com>
2024-04-21 16:34:25 +0500

    Vertical Table Placing is also done!
    You can choose horizontal and vertical using enum!

--- Assets/Features/IsometricGrid2D/Scripts/PlacementManager.cs
+++ Assets/Features/IsometricGrid2D/Scripts/PlacementManager.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public enum TableOrientation
+{
+    Horizontal,
+    Vertical
+}
 public class PlacementManager : MonoBehaviour
 {
-    [SerializeField] int placementIndex = 3;
-    public LayerMask SpriteLayer; // Layer mask for the sprites
+    [SerializeField] int _placementIndex = 3;
+    public LayerMask LayerToRaycast; // Layer mask for the sprites
     public float HoverScaleFactor = 1.2f; // Scale factor for hover animation
-    int _selectedSortingOrder = 1; // Sorting order for the selected tile
     RaycastHit2D _hit;
     Transform _hoveredSprite;
-    SpriteRenderer _hoveredSpriteRenderer;
-    SpriteRenderer _lastSpriteRenderer;
-    private Transform _lastHoveredSprite;
+    Transform _lastHoveredSprite;
     private int _defaultSortingOrder; // Default sorting order of the sprites
     [SerializeField] Transform TablesHolder;
     [SerializeField] private JsonReaderSo JsonReaderSo;
 
-
+    public TableOrientation CurrentOrientation = TableOrientation.Horizontal;
     void Start()
     {
         // Get the default sorting order of the sprites
@@ -31,98 +33,129 @@ public class PlacementManager : MonoBehaviour
 
     void Update()
     {
-        HoverOverGrid();
+        SelectTile();
         CheckTablePlacement();
     }
-    private void HoverOverGrid()
+    private void SelectTile()
     {
-        // Cast a ray from the mouse position to detect the sprites
-        _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, SpriteLayer);
+        _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, LayerToRaycast);
 
-        // If it hits something...
         if (_hit.collider != null)
         {
-            _hoveredSprite = _hit.transform;
-            // Check if the hovered sprite is different from the last one
-            if (_hoveredSprite != _lastHoveredSprite)
+            Transform newHoveredSprite = _hit.transform;
+
+            if (newHoveredSprite != _lastHoveredSprite)
             {
-                // Reset scale of the last hovered sprite
-                if (_lastHoveredSprite != null)
-                {
-                    _lastHoveredSprite.localScale = Vector3.one;
-                    // Reset the sorting order of the last hovered sprite
-                    SpriteRenderer lastSpriteRenderer = _lastHoveredSprite.GetComponent<SpriteRenderer>();
-                    if (lastSpriteRenderer != null)
-                    {
-                        lastSpriteRenderer.sortingOrder = _defaultSortingOrder;
-                    }
-                }
-
-                // Scale up the hovered sprite
-                _hoveredSprite.localScale = Vector3.one * HoverScaleFactor;
-
-                // Set the sorting order of the hovered sprite
-                _hoveredSpriteRenderer = _hoveredSprite.GetComponent<SpriteRenderer>();
-                if (_hoveredSpriteRenderer != null)
-                {
-                    _hoveredSpriteRenderer.sortingOrder = _selectedSortingOrder;
-                }
-
-                // Update last hovered sprite
-                _lastHoveredSprite = _hoveredSprite;
+                ResetLastHoveredSprite();
+                SetHoveredSprite(newHoveredSprite);
             }
+            _lastHoveredSprite = newHoveredSprite;
         }
         else
         {
-            // Reset scale and sorting order of the last hovered sprite when not hovering over any sprite
-            if (_lastHoveredSprite != null)
-            {
-                _lastHoveredSprite.localScale = Vector3.one;
-                _lastSpriteRenderer = _lastHoveredSprite.GetComponent<SpriteRenderer>();
-                if (_lastSpriteRenderer != null)
-                {
-                    _lastSpriteRenderer.sortingOrder = _defaultSortingOrder;
-                }
-                else
-                {
-                    Debug.LogWarning("SpriteRenderer component not found on: " + _lastHoveredSprite.name);
-                }
-                _lastHoveredSprite = null;
-            }
+            ResetLastHoveredSprite();
         }
     }
+
+    private void ResetLastHoveredSprite()
+    {
+        if (_lastHoveredSprite != null)
+        {
+            _lastHoveredSprite.localScale = Vector3.one;
+            var lastSpriteRenderer = _lastHoveredSprite.GetComponent<SpriteRenderer>();
+            if (lastSpriteRenderer != null)
+            {
+                lastSpriteRenderer.sortingOrder = _defaultSortingOrder;
+            }
+            else
+            {
+                Debug.LogWarning("SpriteRenderer component not found on: " + _lastHoveredSprite.name);
+            }
+            _lastHoveredSprite = null;
+        }
+    }
+
+    private void SetHoveredSprite(Transform newHoveredSprite)
+    {
+        _hoveredSprite = newHoveredSprite;
+        _hoveredSprite.localScale = Vector3.one * HoverScaleFactor;
+        var hoveredSpriteRenderer = _hoveredSprite.GetComponent<SpriteRenderer>();
+        if (hoveredSpriteRenderer != null)
+        {
+            hoveredSpriteRenderer.sortingOrder = _defaultSortingOrder + 1;
+        }
+    }
+
     private void CheckTablePlacement()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            if(CheckIfThisTileAvailable(_hoveredSprite.GetComponent<TileInfo>().MyIndex))
+            Vector2Int _selectedTileIndex = _hoveredSprite.GetComponent<TileInfo>().MyIndex;
+            if (CheckIfThisTileAvailable(_selectedTileIndex))
             {
-                int x = _hoveredSprite.GetComponent<TileInfo>().MyIndex.x;
-                int y = _hoveredSprite.GetComponent<TileInfo>().MyIndex.y + 1;
-                Vector2Int nextTileIndex = new Vector2Int(x, y);
-
-                if (IsThisTileEmpty(nextTileIndex)) // Check If next tile is available
+                if (CurrentOrientation == TableOrientation.Horizontal)
                 {
-                    //print("Next Tile is also avaialable : " + _hoveredSprite.GetComponent<TileInfo>().MyIndex);
-                    _hoveredSprite.GetComponent<TileInfo>().Filled = true;
-                    JsonReaderSo.Grid[x, y].GetComponent<TileInfo>().Filled = true;
-                    SpawnTable(_hoveredSprite.GetComponent<TileInfo>().MyIndex);
+                    HorizontalPlacing(_selectedTileIndex);
                 }
-                else
+                else if (CurrentOrientation == TableOrientation.Vertical)
                 {
-                    // Now check if previous tile available
-                    x = _hoveredSprite.GetComponent<TileInfo>().MyIndex.x;
-                    y = _hoveredSprite.GetComponent<TileInfo>().MyIndex.y - 1;
-                    Vector2Int previosTileIndex = new Vector2Int(x, y);
-                    if (IsThisTileEmpty(previosTileIndex))
-                    {
-                        _hoveredSprite.GetComponent<TileInfo>().Filled = true;
-                        JsonReaderSo.Grid[previosTileIndex.x, previosTileIndex.y].GetComponent<TileInfo>().Filled = true;
-                        SpawnTable(previosTileIndex);
-                    }
+                    VerticalPlacing(_selectedTileIndex);
                 }
             }
         }
+    }
+    private void HorizontalPlacing(Vector2Int selectedTileIndex)
+    {
+        int _x = selectedTileIndex.x;
+        int _y = selectedTileIndex.y + 1;
+        Vector2Int nextTileIndex = new Vector2Int(_x, _y);
+
+        if (IsThisTileEmpty(nextTileIndex)) // Check If next tile is available
+        {
+            //print("Next Tile is also avaialable : " + _hoveredSprite.GetComponent<TileInfo>().MyIndex);
+            MarkTilesAsFilled(_x, _y);
+            SpawnTable(selectedTileIndex, CurrentOrientation);
+        }
+        else
+        {
+            // Now check if previous tile available
+            _x = selectedTileIndex.x;
+            _y = selectedTileIndex.y - 1;
+            Vector2Int _previosTileIndex = new Vector2Int(_x, _y);
+            if (IsThisTileEmpty(_previosTileIndex))
+            {
+                MarkTilesAsFilled(_previosTileIndex.x, _previosTileIndex.y);
+                SpawnTable(_previosTileIndex, CurrentOrientation);
+            }
+        }
+    }
+    private void VerticalPlacing(Vector2Int selectedTileIndex)
+    {
+        int _x = selectedTileIndex.x+1;
+        int _y = selectedTileIndex.y; // Offset for vertical placement
+        Vector2Int _nextTileIndex = new Vector2Int(_x, _y);
+
+        if (IsThisTileEmpty(_nextTileIndex)) // Check if the next tile is available
+        {
+            MarkTilesAsFilled(_x, _y);
+            SpawnTable(selectedTileIndex, TableOrientation.Vertical);
+        }
+        else
+        {
+            // Check if the previous tile is available
+            _x = selectedTileIndex.x - 1; // Adjust for previous tile
+            Vector2Int _previousTileIndex = new Vector2Int(_x, _y);
+            if (IsThisTileEmpty(_previousTileIndex))
+            {
+                MarkTilesAsFilled(_previousTileIndex.x, _previousTileIndex.y);
+                SpawnTable(_previousTileIndex, TableOrientation.Vertical);
+            }
+        }
+    }
+    private void MarkTilesAsFilled(int x, int y)
+    {
+        _hoveredSprite.GetComponent<TileInfo>().Filled = true;
+        JsonReaderSo.Grid[x, y].GetComponent<TileInfo>().Filled = true;
     }
     private bool CheckIfThisTileAvailable(Vector2Int index)
     {
@@ -130,7 +163,7 @@ public class PlacementManager : MonoBehaviour
         {
             if (JsonReaderSo.Grid[index.x,index.y].GetComponent<TileInfo>() != null)
             {
-                if(JsonReaderSo.Grid[index.x, index.y].GetComponent<TileInfo>().TileType == placementIndex)
+                if(JsonReaderSo.Grid[index.x, index.y].GetComponent<TileInfo>().TileType == _placementIndex)
                 {
                     if (JsonReaderSo.Grid[index.x, index.y].GetComponent<TileInfo>().Filled == false)
                     {
@@ -153,8 +186,10 @@ public class PlacementManager : MonoBehaviour
         print("Next Tile is not available!");
         return false;
     }
-    private void SpawnTable(Vector2Int index)
+    private void SpawnTable(Vector2Int index, TableOrientation orientation)
     {
-        GameObject table = Instantiate(Resources.Load("HorizontalTable"),JsonReaderSo.Grid[index.x, index.y]) as GameObject;
+        string _prefabName = (orientation == TableOrientation.Horizontal) ? "HorizontalTable" : "VericalTable";
+        GameObject table = Instantiate(Resources.Load(_prefabName), JsonReaderSo.Grid[index.x, index.y]) as GameObject;
     }
+
 }
